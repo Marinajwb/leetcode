d47865e11c18ba42d8504ab8b9fc6375c7aa1362
변지원 <155944342+Marinajwb@users.noreply.github.com>
2025-02-23 18:55:38 +0900

    20250223

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -6,7 +6,7 @@ var isPalindrome = function(x) {
 
   if(x <0 || x%10 == 0 && x!=0 ) 
   return false
-12321
+//음수이거나 10,100,1000 의 경우 false ! 
 
 
   var r = 0;
@@ -16,7 +16,9 @@ var isPalindrome = function(x) {
   }
 
   return x == r || x == Math.floor(r/10) 
-  
+  // 자릿수가 짝수일경우와 홀수일경우 고려해서 
+  // 홀수일경우 마지막 자릿수 버려서 가운데 수를 제외하고 비교 
+  // 단계가 easy인데 애먹었다 갈길이 멀다~ 아잣아잣 
 
  
 };
